93dbbf4428934fb500c7aa7fe88cbe6a28edd002
Boris Goranoff <boris.goranov.bg@gmail.com>
2021-02-03 18:29:13 +0200

    Feat: file system entity interface

---
+++ src/main/java/com/github/bggoranoff/model/files/FileSystemEntity.java
@@ -0,0 +1,18 @@
+package com.github.bggoranoff.model.files;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public interface FileSystemEntity {
+    void delete() throws FileNotFoundException;
+
+    void create() throws IOException;
+
+    void rename(String name) throws IOException;
+
+    void move(String destination, boolean copy) throws IOException;
+
+    long getLastTimeModified();
+
+    java.io.File getContainedFile();
+}
